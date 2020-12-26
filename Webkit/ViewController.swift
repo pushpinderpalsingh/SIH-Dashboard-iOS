6baf04e1c35a54e8183a624b4c933014276192a7
Pushpinder Pal Singh <pushpinderpal19@gmail.com>
2020-12-26 23:59:07 +0530

    Update

---
+++ Webkit/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Webkit
+//
+//  Created by Pushpinder Pal Singh on 01/11/20.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
+    
+    @IBOutlet var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let myURL = URL(string:"http://smart-operator.ml/")
+        let myRequest = URLRequest(url: myURL!)
+        
+        webView.navigationDelegate = self
+        webView.load(myRequest)
+        
+    }
+    
+    @IBAction func BackPressed(_ sender: UIBarButtonItem) {
+        
+        webView.goBack()
+        
+    }
+    
+    @IBAction func forwardPressed(_ sender: UIBarButtonItem) {
+        webView.goForward()
+    }
+    
+    
+}
